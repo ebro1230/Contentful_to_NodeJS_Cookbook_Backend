c6810159d90d1a325444a25469a6fddfdd5ef6ce
ebro1230 <ebro1230@gmail.com>
2023-03-18 11:43:03 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const recipes = require("./routers/recipes");
+const cors = require("cors");
+
+app.use(cors());
+app.use(recipes);
+
+app.listen(PORT, () => {
+  console.log(`Hello.  Listening on port ${PORT}`);
+});
